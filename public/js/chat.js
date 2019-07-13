c1bf361c6cc5a4f256e23f25aec72ee13bc59610
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-07-13 09:23:07 -0400

    enabling and disabling the buttons once the user has sent a message or shared the location and enabling the buttons once the server responds

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,25 +8,50 @@ const socket = io();
 //   socket.emit('increment'); // updating the server that an event happened
 // });
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
 socket.on('message', message => {
   console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', e => {
+$messageForm.addEventListener('submit', e => {
   e.preventDefault();
+  $messageFormButton.setAttribute('disabled', 'disabled');
   // const message = document.querySelector('input').value;
   const message = e.target.elements.message.value;
-  socket.emit('sendMessage', message);
+  // emits the message that the user has typed in the textbox
+  // third param i.e., a callback that will be executed when the server responds with this event
+  socket.emit('sendMessage', message, error => {
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+    console.log('the message was delivered');
+  });
 });
 
-document.querySelector('#send-location').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser');
   }
+  $sendLocationButton.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition(position => {
-    socket.emit('sendLocation', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      () => {
+        console.log('Location shared successfully!');
+        $sendLocationButton.removeAttribute('disabled');
+      }
+    );
   });
 });
--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const Filter = require('bad-words');
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 io.on('connection', socket => {
-  console.log('New web socket connection');
+  // console.log('New web socket connection');
   // socket.emit('countUpdated', count);
   // socket.on('increment', () => {
   //   count = count + 1;
@@ -22,19 +23,25 @@ io.on('connection', socket => {
 
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A new user has joined'); // emits data to all connections available to that server except the current user
-  socket.on('sendMessage', message => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed');
+    }
     io.emit('message', message);
+    callback(); // this is going to acknowledge the client that the server has received the message
   });
   // executed when a user disconnects from the server
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left');
   });
   // listens for sendLocation event and broadcasts that location to the other users connected to this server
-  socket.on('sendLocation', coords => {
+  socket.on('sendLocation', (coords, callback) => {
     io.emit(
       'message',
       `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
     );
+    callback();
   });
 });
 
